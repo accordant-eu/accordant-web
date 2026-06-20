8d91b05c6eb702a01b678a807a359c50d3a216ee
Rufus <rufus@accordant.eu>
2026-06-20 20:23:53 +0000

    fix: correct includes path in .eleventy.js (was ../_includes, must be _includes)

--- .eleventy.js
+++ .eleventy.js
@@ -41,7 +41,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "src",
-      includes: "../_includes",
+      includes: "_includes",
       output: "_site"
     },
     markdownTemplateEngine: "njk",
